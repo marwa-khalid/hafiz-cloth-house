99444b57abc1aa0456485ce0a864b2e623d297f6
ALI HASSAN <83202451+aliii-hassan@users.noreply.github.com>
2023-07-31 05:04:58 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,15 @@ module.exports = {
     return [
       {
         source: '/images/:path*',
-        destination: '/public/images/:path*',
+        destination: './public/images/:path*',
       },
       {
         source: '/layout/css/:path*',
-        destination: '/public/layout/css/:path*',
+        destination: './public/layout/css/:path*',
       },
       {
         source: '/layout/js/:path*',
-        destination: '/public/layout/js/:path*',
+        destination: './public/layout/js/:path*',
       },
     ];
   },
