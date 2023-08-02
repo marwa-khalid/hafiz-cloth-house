efd63602450c63a1923b52e5ce6d8abea22bc63b
ALI HASSAN <alihassanaha110@gmail.com>
2023-08-03 01:25:57 +0500

    Update Aesha Updates

---
+++ Controllers/AboutUsController.js
@@ -0,0 +1,7 @@
+const AboutUsPage = async (request, response, next) => 
+{
+    response.render("./AboutUs");
+}
+module.exports = {
+    AboutUsPage
+};
--- Protocols/Routes/PublicRoutes.js
+++ Protocols/Routes/PublicRoutes.js
@@ -4,14 +4,22 @@ const router = express.Router();
 const HomePageController = require("../../Controllers/HomePageController");
 const ProductsController = require("../../Controllers/ProductsController");
 const ProductViewController = require("../../Controllers/ProductViewController");
-
+const AboutUsController = require("../../Controllers/AboutUsController");
+const ViewCartController = require("../../Controllers/ViewCartController");
+const BillingInfoController = require("../../Controllers/BillingInfoController");
 const { HomePage } = HomePageController;
 const { ProductsPage } = ProductsController;
 const { ProductViewPage } = ProductViewController;
+const { AboutUsPage } = AboutUsController;
+const { ViewCartPage } = ViewCartController;
+const { BillingInfoPage } = BillingInfoController;
 
 router.get("/", HomePage);
 router.get("/products", ProductsPage);
 router.post("/products", ProductsPage);
 router.get("/productview", ProductViewPage);
+router.get("/aboutus", AboutUsPage);
+router.get("/viewcart", ViewCartPage);
+router.get("/billinginfo", BillingInfoPage);
 
 module.exports = router;
